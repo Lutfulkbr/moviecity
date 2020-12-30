1640963cb74bc5fefa9f079210d67d43d05f91e0
github username <github email address>
2020-12-30 15:10:23 -0500

    add change to gem

--- lib/api.rb
+++ lib/api.rb
@@ -2,11 +2,20 @@ class API
 
     def self.fetch_movies
         url = "https://ghibliapi.herokuapp.com/films"
-        binding.pry
         uri = URI(url)
         response = Net::HTTP.get(uri)
+
         array_of_movies = JSON.parse(response)
-        
+
+        array_of_movies.each do |movie_hash|
+            movie = Movie.new
+            movie.title = movie_hash["title"]
+            movie.description = movie_hash["description"]
+            movie.director = movie_hash["director"]
+            movie.producer = movie_hash["producer"]
+            movie.release_date = movie_hash["release_date"]
+            movie.rt_score = movie_hash["rt_score"]
+        end
     end
 
 end
--- lib/movie.rb
+++ lib/movie.rb
@@ -1,3 +1,14 @@
 class Movie
+    attr_accessor :title, :description, :director, :producer, :release_date, :rt_score
+
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
